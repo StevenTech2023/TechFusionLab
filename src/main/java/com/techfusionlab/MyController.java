e7af07f77a4cb48bfb65766afcb2a31958dba33a
Steven <steventech0907@gmail.com>
2023-06-19 17:16:52 +0800

    add remote url read conf

--- src/main/java/com/techfusionlab/MyController.java
+++ src/main/java/com/techfusionlab/MyController.java
@@ -12,7 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
@@ -28,7 +32,10 @@ public class MyController {
     @Value("${password}")
     private String password;
 
-    public static final String FLAG_CLASH = "clash";
+    @Value("${clashUrl}")
+    private String clashUrl;
+
+    public static final String FLGA_CLASH = "clash";
 
     @GetMapping("/hello")
     public String hello() {
@@ -43,21 +50,64 @@ public class MyController {
                     .body("");
         }
 
-        // 构建资源路径
-        String fileName = "shadowrocket-subscribe.yaml";
-        if (FLAG_CLASH.equals(flag)) {
-            fileName = "clash-subscribe.yaml";
+//        if (FLGA_CLASH.equals(flag)) {
+//            String content = sendHttpRequest(clashUrl);
+//            return ResponseEntity.ok()
+//                    .contentType(MediaType.TEXT_HTML)
+//                    .body(content);
+//        }
+
+        if (FLGA_CLASH.equals(flag)) {
+            String filePath = "/file/clash-subscribe.yaml";
+            Resource resource = new ClassPathResource(filePath);
+            String content = StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
+            return ResponseEntity.ok()
+                    .contentType(MediaType.TEXT_PLAIN)
+                    .body(content);
         }
-        String filePath = "/file/" + fileName;
-        // 从资源文件中获取内容
+
+
+        String filePath = "/file/shadowrocket-subscribe.yaml";
         Resource resource = new ClassPathResource(filePath);
         String content = StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
-        if (!FLAG_CLASH.equals(flag)) {
-            content = Base64.getEncoder().encodeToString(content.getBytes(StandardCharsets.UTF_8));
-        }
+        content = Base64.getEncoder().encodeToString(content.getBytes(StandardCharsets.UTF_8));
         // 返回内容作为响应
         return ResponseEntity.ok()
                 .contentType(MediaType.TEXT_PLAIN)
                 .body(content);
     }
+
+    private String sendHttpRequest(String url) throws IOException {
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+
+        try {
+            URL apiUrl = new URL(url);
+            connection = (HttpURLConnection) apiUrl.openConnection();
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
+                StringBuilder response = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                    response.append("\n");
+                }
+                return response.toString();
+            } else {
+                // 处理请求失败的情况
+                return "";
+            }
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+    }
+
 }
