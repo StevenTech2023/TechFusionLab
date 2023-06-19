08c0f39ab570df4397f630dee138b2355262511f
Tjx <xingxing200308@gmail.com>
2023-06-19 21:38:05 +0800

    add test

--- src/main/java/com/techfusionlab/MyController.java
+++ src/main/java/com/techfusionlab/MyController.java
@@ -39,7 +39,7 @@ public class MyController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, World!";
+        return "Hello, World! Branch Test";
     }
 
     @GetMapping("/v1/client/subscribe")
