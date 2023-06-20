9b4ece14049e6a485a524143a03fefa4d30eae9c
Tjx <xingxing200308@gmail.com>
2023-06-20 18:30:20 +0800

    add comment

--- src/main/java/com/techfusionlab/MyController.java
+++ src/main/java/com/techfusionlab/MyController.java
@@ -39,7 +39,7 @@ public class MyController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, World!";
+        return "Hello, Worldssssss!";
     }
 
     @GetMapping("/v1/client/subscribe")
